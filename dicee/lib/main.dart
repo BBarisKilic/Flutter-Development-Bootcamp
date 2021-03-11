37397fede9e7633435b026eefe73a288f45ebe8c
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2021-03-11 14:23:17 +0300

    Dicee UI Completed.

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
+import 'package:flutter/src/widgets/basic.dart';
+import 'package:flutter/src/widgets/basic.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -18,6 +22,25 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  print("left button pressed!");
+                },
+                child: Image.asset('images/dice1.png')),
+          ),
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  print("right button pressed!");
+                },
+                child: Image.asset('images/dice1.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
