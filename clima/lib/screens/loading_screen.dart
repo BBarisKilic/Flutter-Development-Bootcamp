c23851634e5944e03c8a075b819b54ec0f2bcd5b
BBarisKilic <bulentbariskilic@gmail.com>
2021-03-24 23:55:31 +0300

    Location plugin added.

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +9,47 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+  }
+
+  getWeatherData() async {
+    Uri url = Uri.https('samples.openweathermap.org', 'data/2.5/weather', {
+      'lat': '35',
+      'lon': '139',
+      'appid': '439d4b804bc8187953eb36d2a8c26a02'
+    });
+
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+
+      double temp = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String name = decodedData['name'];
+
+      print(name);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    getWeatherData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
